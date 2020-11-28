c26f7df9acd2a7f43515e4b9e9ff371b4109b257
Ahmed Softev <ahmed_v@abv.bg>
2020-11-28 19:15:07 +0200

    C# OOP

---
+++ ValidationAttributes/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ValidationAttributes
+{
+    public class Person
+    {
+        [MyRange(12, 90)]
+       // [Range(12, 90)]
+        public int Age { get; set; }
+
+        [MyRequired]
+        //[Required]
+        public string FullName { get; set; }
+
+        public Person(string fullName, int age)
+        {
+            this.FullName = fullName;
+            this.Age = age;
+        }
+
+    }
+}
---
+++ ValidationAttributes/Validator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ValidationAttributes
+{
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            PropertyInfo[] properties = obj.GetType().GetProperties();
+
+            foreach (PropertyInfo property in properties)
+            {
+                IEnumerable<MyValidationAttribute> propertyCustomAttribute = property.GetCustomAttributes()
+                    .Where(x => x is MyValidationAttribute)
+                    .Cast<MyValidationAttribute>();
+
+                foreach (MyValidationAttribute attribute in propertyCustomAttribute)
+                {
+                    bool result = attribute.IsValid(property.GetValue(obj));
+
+                    if (!result)
+                    {
+                        return false;
+                    }
+
+                }
+            }
+
+            return true;
+        }
+    }
+}
